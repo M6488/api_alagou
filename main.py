27e172bb89214998b92d8a3eec5501eec4ae699f
M6488 <88990286+M6488@users.noreply.github.com>
2025-05-20 22:03:48 +0000

    Renomeia main para main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from fastapi import FastAPI, Request
+import psycopg2
+
+app = FastAPI()
+
+conn = psycopg2.connect(
+    dbname=os.environ["DB_NAME"],
+    user=os.environ["DB_USER"],
+    password=os.environ["DB_PASS"],
+    host=os.environ["DB_HOST"],
+    port=os.environ["DB_PORT"]
+)
+cursor = conn.cursor()
+
+@app.post("/receber-dado")
+async def receber_dado(request: Request):
+    data = await request.json()
+    for feed in data.get("feeds", []):
+        cursor.execute("""
+            INSERT INTO leituras_iot (
+                created_at, valor_ads_bateria, tensao_bateria,
+                valor_ads_hs, tensao_hs, corrente_hs, nivel_agua
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """, (
+            feed['created_at'],
+            float(feed['field1']),
+            float(feed['field2']),
+            float(feed['field5']),
+            float(feed['field6']),
+            float(feed['field7']),
+            float(feed['field8'])
+        ))
+        conn.commit()
+
+    return {"status": "ok"}
