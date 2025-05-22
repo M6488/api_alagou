5da448d5b31350816024a876fb652bb3f5be9577
M6488 <88990286+M6488@users.noreply.github.com>
2025-05-22 16:14:21 +0000

    mudei a leitura do json

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import os
+import json
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
+import os
 import pprint
-import json
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -13,6 +14,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 conn = psycopg2.connect(
     dbname=os.environ["DB_NAME"],
     user=os.environ["DB_USER"],
@@ -31,18 +33,25 @@ def parse_float(value):
 @app.post("/receber-dado")
 async def receber_dado(request: Request):
     raw_body = await request.body()
-    text = raw_body.decode("utf-8")
+    body_str = raw_body.decode("utf-8")
+    json_parts = []
+    temp = ""
+    open_braces = 0
+    for char in body_str:
+        if char == '{':
+            open_braces += 1
+        elif char == '}':
+            open_braces -= 1
 
-    if not text.strip():
-        return {"erro": "Requisição vazia"}
+        temp += char
 
-    blocos_json = [b for b in text.split("\n") if b.strip()]
-    processados = 0
-    ignorados = 0
+        if open_braces == 0 and temp.strip():
+            json_parts.append(temp.strip())
+            temp = ""
 
-    for bloco in blocos_json:
+    for json_part in json_parts:
         try:
-            data = json.loads(bloco)
+            data = json.loads(json_part)
         except json.JSONDecodeError as e:
             print(f"Erro ao decodificar um dos JSONs: {e}")
             continue
@@ -63,9 +72,11 @@ async def receber_dado(request: Request):
             corrente_hs = parse_float(feed.get("field7"))
             nivel_agua = parse_float(feed.get("field8"))
 
-            if None in [valor_ads_bateria, tensao_bateria, valor_ads_hs, tensao_hs, corrente_hs, nivel_agua]:
+            if None in [
+                valor_ads_bateria, tensao_bateria,
+                valor_ads_hs, tensao_hs, corrente_hs, nivel_agua
+            ]:
                 print("Feed ignorado por dados inválidos:", feed)
-                ignorados += 1
                 continue
 
             cursor.execute("""
@@ -82,6 +93,5 @@ async def receber_dado(request: Request):
                 nome_canal, latitude, longitude
             ))
             conn.commit()
-            processados += 1
 
-    return {"status": "ok", "leituras_processadas": processados, "leituras_ignoradas": ignorados}
+    return {"status": "ok"}
